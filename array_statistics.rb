fdaac647758ecac0d532f911e734e237e5841c93
Zach Young <zachjyoung@gmail.com>
2013-09-06 11:23:04 -0400

    initial commit, user stories added

---
+++ array_statistics.rb
@@ -0,0 +1,41 @@
+# As an array analyst
+# I want to know the largest number in an array
+
+# As an array analyst
+# I want to know the smallest number in an array
+
+# As an array analyst
+# I want to know the average of all numbers in the array
+
+# As an array analyst
+# I want to know the standard deviation of all numbers in the array
+
+# As an array analyst
+# I want to know the nth smallest number in the array
+
+# Sample Output
+# a = ArrayStatistic.new([1, 1000, 200, 300, 3000, 4000, 5500])
+# a.nth_smallest(3)
+
+# => 300
+# As an array analyst
+# I want to know the sum of all of the numbers in the array
+
+# As an array analyst
+# I want to know the product of all the numbers in the array
+
+# As an array analyst
+# I want to know the difference between the highest and lowest number in the array
+
+# As an array analyst
+# I want a hash that breaks down how many times n-digit numbers are found in the array
+
+# Example for the above user story:
+
+# ArrayStatistic.new([1, 1000, 200, 300, 3000, 4000, 5500]).digit_frequency
+
+# => {
+#   1: 1,
+#   2: 2,
+#   3: 4
+# }
